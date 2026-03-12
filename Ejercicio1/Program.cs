928bff3ee5ebb18592aff54e2cfe0241c630ace0
MiqueasCast <castmicah@gmail.com>
2026-03-12 09:51:06 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,16 @@
+﻿int Sumar(int a, int b)
+{
+    return a + b;
+}
+
+Console.Write("Ingrese primer número: ");
+int numero1 = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese segundo número: ");
+int numer2 = int.Parse(Console.ReadLine());
+
+
+
+int resultado = Sumar(numero1,numer2);
+
+Console.WriteLine("Resultado:" + resultado);
---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+int Cuadrado(int a)
+{
+    return a * a;
+}
+
+
+Console.Write("Ingrese un número: ");
+int numero = int.Parse(Console.ReadLine());
+
+int resultado = Cuadrado(numero);
+
+Console.WriteLine($"Resultado: {resultado}");
