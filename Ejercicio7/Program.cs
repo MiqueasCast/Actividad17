b43e899d39d8ef50ee7f277e451ae111cc6b70a3
MiqueasCast <castmicah@gmail.com>
2026-03-12 21:28:13 -0600

    Nuevos ejercicios

---
+++ Ejercicio7/Program.cs
@@ -0,0 +1,18 @@
+﻿// Crear una función que reciba el precio de un producto y el porcentaje de descuento y devuelva el precio final.
+
+Console.Write("Ingrese precio del producto: ");
+int precio = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese porcentaje del descuento: ");
+double descuento = int.Parse(Console.ReadLine());
+
+int PrecioFinal(int a, double b)
+{
+    double calculo = descuento / 100;
+    double resultado = a - (a * calculo);
+    return (int)resultado;
+}
+
+double resultado = PrecioFinal(precio, descuento);
+
+Console.WriteLine("Precio final: " + resultado);
---
+++ Ejercicio8/Program.cs
@@ -0,0 +1,24 @@
+﻿// Crear una función que reciba tres números y devuelva el promedio de los tres
+
+double Promedio(int a, int b)
+{
+    double promedio = a / b;
+    return promedio;
+}
+
+int contador = 3;
+int sumador = 0;
+int indiceNumero = 1;
+
+for (int i = 1; i <= contador; i++)
+{
+    Console.Write($"Ingrese número {indiceNumero}: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    indiceNumero++;
+    sumador += numero;
+}
+
+double resultado = Promedio(sumador, contador);
+
+Console.WriteLine("Promedio: " + resultado);
